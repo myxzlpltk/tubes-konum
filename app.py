147c78580a9a4f46733d7cd0d45c30706ff91136
Saddam Azy <myxzlpltk@gmail.com>
2021-05-04 11:26:43 +0700

    basic index page

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pandas as pd
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    data = pd.read_csv("data/dataset.csv")
+    data = data[['tahun', 'jumlah_pengguna_internet']]
+    return render_template('index.html', data=data.to_dict())
 
+@app.route('/predictions', methods=['POST'])
+def predictions():
+    data = pd.DataFrame({
+        'tahun': request.form.getlist('tahun[]'),
+        'jumlah_pengguna_internet': request.form.getlist('jumlah_pengguna_internet[]')
+    })
+    return data.to_html()
 
 if __name__ == '__main__':
     app.run(debug=True)
