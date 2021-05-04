a43a906a6554bc435aa7c3276a1dfb3cc0085d7c
Saddam Azy <myxzlpltk@gmail.com>
2021-05-04 13:09:55 +0700

    predictions page

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request
-import pandas as pd
+import base64
+import io
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import locale
+
+locale.setlocale(locale.LC_ALL, 'id_ID')
 
 app = Flask(__name__)
 
@@ -14,29 +20,42 @@ def index():
 
 @app.route('/predictions', methods=['POST'])
 def predictions():
-    tahun_prediksi_awal = int(request.form.get('min_year'))
-    tahun_prediksi_akhir = int(request.form.get('max_year'))
-    range_predictions = [x for x in range(tahun_prediksi_awal, tahun_prediksi_akhir+1)]
+    min_year = int(request.form.get('min_year'))
+    max_year = int(request.form.get('max_year'))
+    range_predictions = np.arange(min_year, max_year+1, 1)
 
     data = pd.DataFrame({
         'tahun': request.form.getlist('tahun[]'),
         'jumlah_pengguna_internet': request.form.getlist('jumlah_pengguna_internet[]')
-    }).astype({
-    'tahun': 'int64',
-    'jumlah_pengguna_internet': 'int64',
-    })
-    print(data['tahun'])
-    polynoms = np.polyfit(data['tahun'], data['jumlah_pengguna_internet'], ORDE_POLINOMIAL)
-    model = np.poly1d(polynoms)
+    }).astype({'tahun': 'int64', 'jumlah_pengguna_internet': 'int64'})
+
+    X = data['tahun']
+    y = data['jumlah_pengguna_internet']
+
+    model = np.poly1d(np.polyfit(X, y, ORDE_POLINOMIAL))
+
     predictions = model(range_predictions)
-    df_pred = pd.DataFrame({
+    result = pd.DataFrame({
         'tahun': range_predictions,
         'jumlah_pengguna_internet': [round(x) for x in predictions]
-    })
-    print(df_pred)
-    data = data.append(df_pred, ignore_index=True)
+    }).sort_values(by='tahun')
 
-    return data.to_html()
+    # Plot
+    new_X = np.concatenate([X, range_predictions])
+    new_y = np.concatenate([y, predictions])
+    plt.scatter(new_X, new_y, color='blue')
+    line = np.linspace(X.min(), range_predictions.max(), 100)
+    plt.plot(line, model(line), color='red')
+    plt.title('Polynomial Regression')
+    plt.xlabel('Tahun')
+    plt.ylabel('Jumlah Pengguna Internet')
+
+    s = io.BytesIO()
+    plt.savefig(s, format='png', bbox_inches="tight")
+    plt.close()
+    s = base64.b64encode(s.getvalue()).decode("utf-8").replace("\n", "")
+
+    return render_template('predictions.html', plot=s, data=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
